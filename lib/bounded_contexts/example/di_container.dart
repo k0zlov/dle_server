a5331d5c71f087e3bba5196fe7bb41614ec30a48
Ivan Kozlov <118103201+k0zlov@users.noreply.github.com>
2025-03-05 20:31:55 +0100

    Creating context example folder

---
+++ lib/bounded_contexts/example/di_container.dart
@@ -0,0 +1,52 @@
+import 'package:dle_server/di_container/register_dependencies.dart';
+import 'package:dle_server/shared_kernel/adapters/secondary/event_bus/in_memory_event_bus.dart';
+import 'package:dle_server/shared_kernel/application/ports/event_bus.dart';
+import 'package:dle_server/shared_kernel/infrastructure/events/event_listener.dart';
+import 'package:uuid/v4.dart';
+
+final String _contextId = const UuidV4().generate();
+
+Future<void> registerExampleDependencies() async {
+  _repositories();
+  _services();
+  _eventBus();
+  _useCases();
+  _listeners();
+  _controllers();
+  _routes();
+}
+
+void _repositories() {}
+
+void _services() {}
+
+void _eventBus() {
+  getIt.registerLazySingleton<EventBus>(
+    instanceName: _contextId,
+    InMemoryEventBus.new,
+  );
+}
+
+void _useCases() {
+  final EventBus domainEventBus = getIt(instanceName: _contextId);
+}
+
+void _listeners() {
+  final List<DomainEventListener> domainListeners = [];
+
+  final EventBus domainEventBus = getIt(instanceName: _contextId);
+
+  for (final DomainEventListener listener in domainListeners) {
+    domainEventBus.subscribe(listener.call, eventType: listener.eventType);
+  }
+
+  final List<IntegrationEventListener> integrationListeners = [];
+
+  for (final IntegrationEventListener listener in integrationListeners) {
+    getIt.registerLazySingleton<IntegrationEventListener>(() => listener);
+  }
+}
+
+void _controllers() {}
+
+void _routes() {}
